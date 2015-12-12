f4a8339174326931ff38d2a82a683bdac9e37d0b
Diego Coelho <diegocoelho.13@gmail.com>
2015-12-12 16:55:43 -0200

    Botao

--- js/starbem.js
+++ js/starbem.js
@@ -1,12 +1,12 @@
-//Parse.initialize("bck5MqFK8Xm5LK6TBuaSec2LRaVsz9Bff4cV55W2", "iONuVeJEeqF1RCoLXCXBciFxntpUk1k7AFKfshEo");
+Parse.initialize("bck5MqFK8Xm5LK6TBuaSec2LRaVsz9Bff4cV55W2", "iONuVeJEeqF1RCoLXCXBciFxntpUk1k7AFKfshEo");
 
 var x = 0, y = 0, z = 0;
    vx = 0, vy = 0, vz = 0,
    ax = 0, ay = 0, az = 0;
-
+var STARted = false;
 var sphere = document.getElementById("sphere");
-// var Estrelinhas = Parse.Object.extend("estrelinhas");
-// var estrelinha = new Estrelinhas();
+ var Estrelinhas = Parse.Object.extend("estrelinhas");
+ var estrelinha = new Estrelinhas();
 
 if (window.DeviceMotionEvent != undefined) {
    window.ondevicemotion = function(e) {
@@ -15,14 +15,14 @@ if (window.DeviceMotionEvent != undefined) {
        az = e.acceleration.z;
 
        //e.acceleration
-       document.getElementById("accelerationX").innerHTML = ax;
-       document.getElementById("accelerationY").innerHTML = ay;
-       document.getElementById("accelerationZ").innerHTML = az;
+       document.getElementById("accelerationX").innerHTML = ax.toFixed(2);
+       document.getElementById("accelerationY").innerHTML = ay.toFixed(2);
+       document.getElementById("accelerationZ").innerHTML = az.toFixed(2);
 
        if ( e.rotationRate ) {
-           document.getElementById("rotationAlpha").innerHTML = e.rotationRate.alpha;
-           document.getElementById("rotationBeta").innerHTML = e.rotationRate.beta;
-           document.getElementById("rotationGamma").innerHTML = e.rotationRate.gamma;
+           document.getElementById("rotationAlpha").innerHTML = e.rotationRate.alpha.toFixed(2);
+           document.getElementById("rotationBeta").innerHTML = e.rotationRate.beta.toFixed(2);
+           document.getElementById("rotationGamma").innerHTML = e.rotationRate.gamma.toFixed(2);
        }
    }
 
@@ -43,22 +43,34 @@ if (window.DeviceMotionEvent != undefined) {
        vx = vx * decay;
        vy = vy * decay;
        vz = vz * decay;
-       document.getElementById("vx").innerHTML = vx;
-       document.getElementById("vy").innerHTML = vy;
-       document.getElementById("vz").innerHTML = vz;
+       document.getElementById("vx").innerHTML = vx.toFixed(2);
+       document.getElementById("vy").innerHTML = vy.toFixed(2);
+       document.getElementById("vz").innerHTML = vz.toFixed(2);
 
        // velocidade --> posição
        y = parseInt(y + vy);
        x = parseInt(x + vx);
        z = parseInt(z + vz);
 
-       //estrelinha.save({x_value: x, y_value:y, z_value : z});
        boundingBoxCheck();
 
        sphere.style.top = y + "px";
        sphere.style.left = x + "px";
-       console.log("dashdiashfkasud")
+       if(STARted){
+          estrelinha.save({x_value: x, y_value:y, z_value : z, starid: 'estrelinha2'});
+       }
+
    }, 50);
+}
+
+function brilha(){
+    if(STARted){
+        STARted = false;
+        document.getElementById("startButton").innerHTML = "Start"
+    } else {
+        STARted = true;
+        document.getElementById("startButton").innerHTML = "Stop"
+    }
 }
 
 
